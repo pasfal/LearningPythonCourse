fdfd0d5e293553a7a19a7380b3f721ce084b8ec8
Pasquale Falvo <pasfal@outlook.com>
2021-04-10 17:12:21 +0200

    Added exercise

--- CorsoPython3.9/conto1.py
+++ CorsoPython3.9/conto1.py
@@ -2,16 +2,25 @@ class ContoCorrente:
     def __init__(self, nome, conto, importoIniziale):
         self.nome = nome
         self.conto = conto
-        self.saldo = importoIniziale
+        self.__saldo = importoIniziale
     
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @saldo.setter
+    def saldo(self, nuovoSaldo):
+       self.preleva(self.__saldo)
+       self.deposita(nuovoSaldo)
+
     def preleva(self, importoPrelevato):
-        self.saldo -= importoPrelevato
+        self.__saldo -= importoPrelevato
     
     def deposita(self, importoDaDepositare):
-        self.saldo += importoDaDepositare
+        self.__saldo += importoDaDepositare
     
     def descrizione(self):
-        print(f"Nome titolare del conto: {self.nome} - Numero conto: {self.conto} - Saldo: {self.saldo}")
+        print(f"Nome titolare del conto: {self.nome} - Numero conto: {self.conto} - Saldo: {self.__saldo}")
 
 contoCorrente1 = ContoCorrente("Mario Rossi", "IT12345", 150.25)
 contoCorrente2 = ContoCorrente("Anna Verdi", "IT57432", 650.70)
@@ -23,8 +32,13 @@ contoCorrente1.preleva(25)
 contoCorrente1.descrizione()
 contoCorrente1.deposita(450.75)
 contoCorrente1.descrizione()
+print(f"\nValore della property Saldo per Mario Rossi: {contoCorrente1.saldo} \n")
 
 contoCorrente2.preleva(50.70)
 contoCorrente2.descrizione()
 contoCorrente2.deposita(400)
 contoCorrente2.descrizione()
+print(f"\nValore della property Saldo per Anna Verdi: {contoCorrente2.saldo} \n")
+
+#contoCorrente1.saldo = 5000
+#print(contoCorrente1.saldo)
