eb4f8a5c1f6ec242f3966fb8fcddf204827afeb2
Pasquale Falvo <pasfal@outlook.com>
2021-05-29 16:58:38 +0200

    Test commit

--- BasicPythonCourseLearning/conto1.py
+++ BasicPythonCourseLearning/conto1.py
@@ -47,6 +47,7 @@ contoCorrente1.deposita(450.75)
 contoCorrente1.descrizione()
 print(f"\nValore della property Saldo per Mario Rossi: {contoCorrente1.saldo} \n")
 
+
 contoCorrente2.preleva(50.70)
 contoCorrente2.descrizione()
 contoCorrente2.deposita(400)
@@ -55,6 +56,3 @@ print(f"\nValore della property Saldo per Anna Verdi: {contoCorrente2.saldo} \n"
 
 GestoreContiCorrenti.bonifico(contoCorrente2, contoCorrente1, 200)
 print(f"Saldo conto corrente #1: {contoCorrente1.saldo} - Saldo conto corrente #2: {contoCorrente2.saldo}")
-
-#contoCorrente1.saldo = 5000
-#print(contoCorrente1.saldo)
